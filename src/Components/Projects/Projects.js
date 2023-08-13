06996226b58ad762e3eeec4ba1aac2a3c7583bbe
TonyH98 <tonyhoang@pursuit.org>
2023-08-12 21:50:06 -0400

    Reworking Project section

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -44,34 +44,28 @@ return(
                 <h2 className="project-header">1. Kick.It</h2>
 
                 <p className="project-descript">
-                Kick.it is our collaborative event hosting app, developed as a group project where I led the backend development.
-
-                 Powered by the PERN stack, Kick.it empowers users to create and host events,
-                  add comments, invite co-hosts, send friend requests, RSVP, and enjoy real-time messaging with Socket.IO.
-                   With a focus on seamless event management, Kick.it demonstrates my backend expertise in a team environment, 
-                   showcasing my ability to deliver engaging and feature-rich applications. 
                 </p>
                 <span style={{"fontWeight": "bold"}}>Front-End Tools:</span>
                    <div className="tools-container">
                     {frontend1.map((front) => {
-                    return(
-                        <div className="tools" key={front}>
+                        return(
+                            <div className="tools" key={front}>
                             {front}
                         </div>
                     )
-                   })}
+                })}
 
                    </div>
                   
                    <span style={{"fontWeight": "bold"}}>Back-End Tools:</span>
                    <div className="tools-container">
                    {backend1.map((back) => {
-                    return(
-                        <div className="tools" key={back}>
+                       return(
+                           <div className="tools" key={back}>
                             {back}
                         </div>
                     )
-                   })}
+                })}
 
                    </div>
 
@@ -79,12 +73,12 @@ return(
                    <span style={{"fontWeight": "bold"}}>Other Tools:</span>
                    <div className="tools-container">
                    {other1.map((other) => {
-                    return(
-                        <div className="tools" key={other}>
+                       return(
+                           <div className="tools" key={other}>
                             {other}
                         </div>
                     )
-                   })}
+                })}
 
                    </div>
         </div>
@@ -112,16 +106,7 @@ return(
 
                 <h2 className="project-header2">2. Collection Vault</h2>
                     <p className="project-descript">
-                    Introducing Collection Vault, my solo-developed e-commerce app designed for 
-                    seamless mobile shopping experiences. Built using the PERN stack, 
-                    Collection Vault offers a diverse range of products, including video games, 
-                    comics, and manga. With user registration, cart management, favorites, 
-                    search history, purchase tracking, reviews, and secure payments through 
-                    the Stripe API, Collection Vault provides a comprehensive shopping solution. 
-                    Explore its sleek and user-friendly interface, showcasing my expertise 
-                    in full-stack development and creating engaging user experiences. 
-                    Collection Vault is a testament to my skills as a software developer, 
-                    combining functionality, usability, and a visually appealing design.
+                   
 
                         
                      
@@ -130,12 +115,12 @@ return(
                     <span style={{"fontWeight": "bold"}}>Front-End Tools:</span>
                         <div className="tools-container">
                    {frontend2.map((front) => {
-                    return(
-                        <div className="tools" key={front}>
+                       return(
+                           <div className="tools" key={front}>
                             {front}
                         </div>
                     )
-                   })}
+                })}
 
                    </div>
                         
@@ -144,12 +129,12 @@ return(
 
                         <div className="tools-container">
                    {backend1.map((back) => {
-                    return(
-                        <div className="tools" key={back}>
+                       return(
+                           <div className="tools" key={back}>
                             {back}
                         </div>
                     )
-                   })}
+                })}
 
                    </div>
 
@@ -157,12 +142,12 @@ return(
                    <span style={{"fontWeight": "bold"}}>Other Tools:</span>
                    <div className="tools-container">
                    {other2.map((other) => {
-                    return(
-                        <div className="tools" key={other}>
+                       return(
+                           <div className="tools" key={other}>
                             {other}
                         </div>
                     )
-                   })}
+                })}
 
                    </div>
 
@@ -183,3 +168,22 @@ return(
 }
 
 export default Projects
+{/* Kick.it is our collaborative event hosting app, developed as a group project where I led the backend development.
+
+ Powered by the PERN stack, Kick.it empowers users to create and host events,
+  add comments, invite co-hosts, send friend requests, RSVP, and enjoy real-time messaging with Socket.IO.
+   With a focus on seamless event management, Kick.it demonstrates my backend expertise in a team environment, 
+   showcasing my ability to deliver engaging and feature-rich applications.  */}
+
+
+
+    {/* Introducing Collection Vault, my solo-developed e-commerce app designed for 
+                    seamless mobile shopping experiences. Built using the PERN stack, 
+                    Collection Vault offers a diverse range of products, including video games, 
+                    comics, and manga. With user registration, cart management, favorites, 
+                    search history, purchase tracking, reviews, and secure payments through 
+                    the Stripe API, Collection Vault provides a comprehensive shopping solution. 
+                    Explore its sleek and user-friendly interface, showcasing my expertise 
+                    in full-stack development and creating engaging user experiences. 
+                    Collection Vault is a testament to my skills as a software developer, 
+                    combining functionality, usability, and a visually appealing design. */}
