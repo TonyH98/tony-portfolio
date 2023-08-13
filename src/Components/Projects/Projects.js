5686c07346533ea8f850724383058920aed09006
TonyH98 <tonyhoang@pursuit.org>
2023-08-13 11:42:52 -0400

    Add Description for the projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -18,17 +18,30 @@ return(
 
         <h2>1. Kick.It</h2>
         <img src="./kickit.png" alt="kickit" className="project-img"/>
+        <div class="project-content">
+        <p>
+            Introducing kickit: your all-in-one event solution. 
+            Plan, host, and join events effortlessly. Enjoy real-time attendee messaging for
+            seamless communication. Kickit streamlines registration, tracks attendance,
+             and gathers feedback for event success. Elevate your events with Kickit today.
+        </p>
+    </div>
         <button className="button-link">Github</button>
         </div>
 
 
         <div className="project">
-        <h2>2. Collection Vault</h2>
-        <img src="./collectionVault.png" alt="collectionValut" className="project-img"/>
-
-        <button className="button-link" >Github</button>
-
-        </div>
+    <h2>2. Collection Vault</h2>
+    <img src="./collectionVault.png" alt="collectionValut" className="project-img"/>
+    <div class="project-content">
+        <p>
+            The app specialized in selling books and comics, users can curate their favorite list, write reviews, 
+            maintain a search history, make secure purchases, and conveniently track their purchase history, creating a 
+            seamless and personalized shopping experience for book and comic enthusiasts.
+        </p>
+    </div>
+        <button className="button-link">Github</button>
+</div>
 
     </div>
 
